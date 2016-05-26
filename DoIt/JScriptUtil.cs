1e976e43ead802448f4a547245e5a69eef42c83d
d8q8 <d8q8@163.com>
2016-05-26 15:39:19 +0800

    调整以前封装的一个JS弹窗插件

--- DoIt/JScriptUtil.cs
+++ DoIt/JScriptUtil.cs
@@ -81,320 +81,69 @@ namespace DoIt
     public class Pop
     {
 
-        #region 基本参数
-        private string _message;
-        private int _width;
-        private int _height;
-        private string _title;
-        private object _handler;
-        private string _maskAlphaColor;
-        private decimal _maskAlpha;
-        private bool _iframe;
-        private string _icoCls;
-        private object _btn;
-        private bool _fixPosition;
-        private bool _autoClose;
-        private bool _dragOut;
-        private bool _titleBar;
-        private bool _showMask;
-        private string _winPos;
-        private decimal _winAlpha;
-        private bool _closeBtn;
-        private bool _showShadow;
-        private bool _useSlide;
-        private object _slideCfg;
-        private string _closeTxt;
-        private string _okTxt;
-        private string _cancelTxt;
-        private string _msgCls;
-        private bool _minBtn;
-        private string _minTxt;
-        private bool _maxBtn;
-        private string _maxTxt;
-        private bool _allowSelect;
-        private bool _allowRightMenu;
-        #endregion
-
-        #region 基本属性
-        /// <summary>
-        /// 消息框按钮
-        /// </summary>
-        public string Message
-        {
-            get { return _message; }
-            set { _message = value; }
-        }
-
-        /// <summary>
-        /// 宽 
-        /// </summary>
-        public int Width
-        {
-            get { return _width; }
-            set { _width = value; }
-        }
-
-        /// <summary>
-        /// 高
-        /// </summary>
-        public int Height
-        {
-            get { return _height; }
-            set { _height = value; }
-        }
-
-        /// <summary>
-        /// 消息框标题
-        /// </summary>
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
-
-        /// <summary>
-        /// 回调事件 
-        /// </summary>
-        public object Handler
-        {
-            get { return _handler; }
-            set { _handler = value; }
-        }
-
-        /// <summary>
-        /// 遮罩透明色 
-        /// </summary>
-        public string MaskAlphaColor
-        {
-            get { return _maskAlphaColor; }
-            set { _maskAlphaColor = value; }
-        }
-
-        /// <summary>
-        /// 遮罩透明度 
-        /// </summary>
-        public decimal MaskAlpha
-        {
-            get { return _maskAlpha; }
-            set { _maskAlpha = value; }
-        }
-
-        /// <summary>
-        /// iframe模式 
-        /// </summary>
-        public bool Iframe
-        {
-            get { return _iframe; }
-            set { _iframe = value; }
-        }
-
-        /// <summary>
-        /// 图标的样式 
-        /// </summary>
-        public string IcoCls
-        {
-            get { return _icoCls; }
-            set { _icoCls = value; }
-        }
-
-        /// <summary>
-        /// 按钮配置 
-        /// </summary>
-        public object Btn
-        {
-            get { return _btn; }
-            set { _btn = value; }
-        }
-
-        /// <summary>
-        /// 点击关闭、确定等按钮后自动关闭 
-        /// </summary>
-        public bool AutoClose
-        {
-            get { return _autoClose; }
-            set { _autoClose = value; }
-        }
-
-        /// <summary>
-        /// 随滚动条滚动 
-        /// </summary>
-        public bool FixPosition
-        {
-            get { return _fixPosition; }
-            set { _fixPosition = value; }
-        }
-
-        /// <summary>
-        /// 不允许拖出窗体范围 
-        /// </summary>
-        public bool DragOut
-        {
-            get { return _dragOut; }
-            set { _dragOut = value; }
-        }
-
-        /// <summary>
-        /// 显示标题栏
-        /// </summary>
-        public bool TitleBar
-        {
-            get { return _titleBar; }
-            set { _titleBar = value; }
-        }
-
-        /// <summary>
-        /// 显示遮罩 
-        /// </summary>
-        public bool ShowMask
-        {
-            get { return _showMask; }
-            set { _showMask = value; }
-        }
-
-        /// <summary>
-        /// 在页面中间显示 
-        /// </summary>
-        public string WinPos
-        {
-            get { return _winPos; }
-            set { _winPos = value; }
-        }
-
-        /// <summary>
-        /// 拖动窗体时窗体的透明度 
-        /// </summary>
-        public decimal WinAlpha
-        {
-            get { return _winAlpha; }
-            set { _winAlpha = value; }
-        }
-
-        /// <summary>
-        /// 是否显示关闭按钮
-        /// </summary>
-        public bool CloseBtn
-        {
-            get { return _closeBtn; }
-            set { _closeBtn = value; }
-        }
-
-        /// <summary>
-        /// 不显示阴影，只对IE有效 
-        /// </summary>
-        public bool ShowShadow
-        {
-            get { return _showShadow; }
-            set { _showShadow = value; }
-        }
-
-        /// <summary>
-        /// 不使用淡入淡出 
-        /// </summary>
-        public bool UseSlide
-        {
-            get { return _useSlide; }
-            set { _useSlide = value; }
-        }
-
-        /// <summary>
-        /// 淡入淡出配置 
-        /// </summary>
-        public object SlideCfg
-        {
-            get { return _slideCfg; }
-            set { _slideCfg = value; }
-        }
-
-        //按钮文本，可通过自定义这些属性实现本地化 
-        /// <summary>
-        /// 关闭按钮
-        /// </summary>
-        public string CloseTxt
-        {
-            get { return _closeTxt; }
-            set { _closeTxt = value; }
-        }
-
-        /// <summary>
-        /// 确定按钮
-        /// </summary>
-        public string OkTxt
-        {
-            get { return _okTxt; }
-            set { _okTxt = value; }
-        }
-
-        /// <summary>
-        /// 取消按钮
-        /// </summary>
-        public string CancelTxt
-        {
-            get { return _cancelTxt; }
-            set { _cancelTxt = value; }
-        }
-
-        /// <summary>
-        /// 消息内容的样式 
-        /// </summary>
-        public string MsgCls
-        {
-            get { return _msgCls; }
-            set { _msgCls = value; }
-        }
-
-        /// <summary>
-        /// 不显示最小化按钮 
-        /// </summary>
-        public bool MinBtn
-        {
-            get { return _minBtn; }
-            set { _minBtn = value; }
-        }
-
-        /// <summary>
-        /// 最少化
-        /// </summary>
-        public string MinTxt
-        {
-            get { return _minTxt; }
-            set { _minTxt = value; }
-        }
-
-        /// <summary>
-        /// 不显示最大化按钮 
-        /// </summary>
-        public bool MaxBtn
-        {
-            get { return _maxBtn; }
-            set { _maxBtn = value; }
-        }
-
-        /// <summary>
-        /// 最大化
-        /// </summary>
-        public string MaxTxt
-        {
-            get { return _maxTxt; }
-            set { _maxTxt = value; }
-        }
-
-        /// <summary>
-        /// 是否允许选择消息框内容，默认不允许 
-        /// </summary>
-        public bool AllowSelect
-        {
-            get { return _allowSelect; }
-            set { _allowSelect = value; }
-        }
-
-        /// <summary>
-        /// 是否允许在消息框使用右键，默认不允许
-        /// </summary>
-        public bool AllowRightMenu
-        {
-            get { return _allowRightMenu; }
-            set { _allowRightMenu = value; }
-        }
+        #region 弹窗基本参数
+        //消息框按钮
+        public string message { get; set; }
+        //宽
+        public int width { get; set; }
+        //高
+        public int height { get; set; }
+        //消息框标题
+        public string title { get; set; }
+        //回调事件
+        public object handler { get; set; }
+        //遮罩透明色
+        public string maskAlphaColor { get; set; }
+        //遮罩透明度
+        public decimal maskAlpha { get; set; }
+        //iframe模式
+        public bool iframe { get; set; }
+        //图标的样式
+        public string icoCls { get; set; }
+        //按钮配置
+        public object btn { get; set; }
+        //随滚动条滚动
+        public bool fixPosition { get; set; }
+        //点击关闭、确定等按钮后自动关闭 
+        public bool autoClose { get; set; }
+        //不允许拖出窗体范围 
+        public bool dragOut { get; set; }
+        //显示标题栏
+        public bool titleBar { get; set; }
+        //显示遮罩 
+        public bool showMask { get; set; }
+        //在页面中间显示 
+        public string winPos { get; set; }
+        //拖动窗体时窗体的透明度
+        public decimal winAlpha { get; set; }
+        //是否显示关闭按钮
+        public bool closeBtn { get; set; }
+        //不显示阴影，只对IE有效
+        public bool showShadow { get; set; }
+        //不使用淡入淡出 
+        public bool useSlide { get; set; }
+        //淡入淡出配置 
+        public object slideCfg { get; set; }
+        //按钮文本，可通过自定义这些属性实现本地化
+        public string closeTxt { get; set; }
+        //确定按钮
+        public string okTxt { get; set; }
+        //取消按钮
+        public string cancelTxt { get; set; }
+        //消息内容的样式 
+        public string msgCls { get; set; }
+        //不显示最小化按钮
+        public bool minBtn { get; set; }
+        //最少化
+        public string minTxt { get; set; }
+        //不显示最大化按钮 
+        public bool maxBtn { get; set; }
+        //最大化
+        public string maxTxt { get; set; }
+        //是否允许选择消息框内容，默认不允许 
+        public bool allowSelect { get; set; }
+        //是否允许在消息框使用右键，默认不允许
+        public bool allowRightMenu { get; set; }
         #endregion
 
     }
